344f4b58ea7441b780dd3f50459c7625d11592af
Shmuel <omgnotthis1@gmail.com>
2022-02-15 09:57:30 +0200

    changed things

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map((person, i)=>
+  // expression goes here:
+  <li key={'person_' + i}>{person}</li>
 );
+
+// ReactDOM.render goes here:
+ReactDOM.render(<ul>{peopleLis}</ul>, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
